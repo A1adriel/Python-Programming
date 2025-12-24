3aaf387bf425c3d42520fcf2c90bbf54dd00d76d
Aladriel <157883842+A1adriel@users.noreply.github.com>
2025-12-24 13:51:56 +0300

    Add files via upload

---
+++ Семестр 5/ЛР-3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="myspackage",  # УНИКАЛЬНОЕ имя на PyPI
+    version="0.1.0",  # Версия - меняется при изменениях
+    author="Sambuev Aldar",  # Ваше имя
+    author_email="sambuev.2002@gmail.com",  # ДЕЙСТВУЮЩИЙ email (ОБЯЗАТЕЛЬНО)
+    description="Flask MVC web application package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jamanuriyeva/JamasPackage",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "Flask>=2.0.0",
+        "Jinja2>=3.0.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'jamaspackage=jamaspackage.main:main',
+        ],
+    },
+)
